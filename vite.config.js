a42d2203ed1e70ed74fe76d17c182bcad5c6c114
Victor Mindrila <victormindrila@gmail.com>
2025-07-26 16:18:44 +0300

    add cache headers

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,47 @@
-import { defineConfig } from "vite";
-import legacy from "@vitejs/plugin-legacy";
-import unocss from "unocss/vite";
-import htmlMinifier from "vite-plugin-html-minifier";
-import webfontDownload from "vite-plugin-webfont-dl";
-import { imagetools } from "vite-imagetools";
+import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
+import unocss from 'unocss/vite';
+import htmlMinifier from 'vite-plugin-html-minifier';
+import webfontDownload from 'vite-plugin-webfont-dl';
+import { imagetools } from 'vite-imagetools';
 
-const HOST = "localhost";
+const HOST = 'localhost';
 const PORT = 3000;
 
 export default defineConfig({
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  },
   preview: {
     headers: {
-      "Cache-Control": "public, max-age=31536000, immutable",
-    },
+      'Cache-Control': 'public, max-age=31536000, immutable'
+    }
   },
   server: {
     host: HOST,
-    port: PORT,
+    port: PORT
   },
   plugins: [
     webfontDownload([
-      "https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap",
-      "https://fonts.googleapis.com/css2?family=Fira+Code&display=swap",
+      'https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap',
+      'https://fonts.googleapis.com/css2?family=Fira+Code&display=swap'
     ]),
     legacy({
-      targets: ["defaults", "not IE 11"],
+      targets: ['defaults', 'not IE 11']
     }),
     unocss(),
     htmlMinifier({
-      minify: true,
+      minify: true
     }),
     imagetools({
-      removeMetadata: true,
-    }),
-  ],
+      removeMetadata: true
+    })
+  ]
 });
